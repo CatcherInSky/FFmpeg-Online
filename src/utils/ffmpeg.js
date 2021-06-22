c2793035ac55dcd21877e4751136e884565cef87
CatcherInSky <1530775669@qq.com>
2021-06-22 17:00:24 +0800

    读取视频信息

--- src/utils/ffmpeg.js
+++ src/utils/ffmpeg.js
@@ -18,27 +18,51 @@ export const initFFmpeg = async (
   return ffmpeg;
 }
 export const getVideoInfo = async ({name, Unit8Array: data}) => {
-  console.log('getInfo', name, data)
-  const res = [], info = {};
-  const logger = (e) => {
-    res.push(e.message)
-  }
-  const ffmpeg = await initFFmpeg(
-    logger
-  );
+  // 正则
+  // 格式 编解码器 文件大小 时长 码率模式 码率 创建日期
+  // 格式 尺寸 帧率模式 帧率 色彩空间 色度抽样 位深 流大小
+  // 格式 码率 声道数 采样率 压缩模式 流大小
+  let res = [], info = {};
+  const logger = (e) => res.push(e.message);
+  const ffmpeg = await initFFmpeg( logger );
   ffmpeg.FS('writeFile', name, data);
+  // ffprobe ?
   await ffmpeg.run('-i', name).then(() => {
-    console.log(res);
-    let flag = false
-    let message = res
-      .filter(it => flag || (flag = it.includes('Metadata')))
-      .filter(it => ['Metadata:', 'At least one output file must be specified', 'FFMPEG_END'].every(t => !it.includes(t)))
-      .join('')
-    console.log(message)
-    // 正则
-    
-
-
+    let flag = false, type = '';
+    try {
+      let message = res
+        .filter(it => flag || (flag = it.includes('Input')))
+        .filter(it => ['Input', 'Metadata:', 'At least one output file must be specified', 'FFMPEG_END'].every(t => !it.includes(t)))
+        .map(it => it.replace(/Stream(.*?)\([a-z]+\):/g, '').trim())
+        .map(it => {
+          if(it.includes('Video')) {
+            type = 'video';
+          } else if(it.includes('Audio')) {
+            type = 'audio'
+          }
+          return type.length > 0 ? `${type}_${it}` : it;
+        });
+      message = `,${message.join(',')},:`.replace(/(?<=,)(.*?):(.*?),(?=\s*\S*\s*:)/g,'"$1":"$2",');
+      message = JSON.parse('{' + message.slice(1, message.length - 2) + '}')
+      Object.keys(message).forEach(key => {
+        if(key.includes('Video')) {
+          // const list = message[key].split(', ');
+          // h264 (Main) (avc1 / 0x31637661), yuv420p(tv, bt709), 1280x720, 10619 kb/s, 30 fps, 30 tbr, 30k 
+          // info.video = {}
+          info.video = message[key].trim();
+        } else if(key.includes('Audio')) {
+          // const [ format, sampling_frequency, sound_track, decode, bit_rate ] = message[key].split(', ');
+          // info.audio = { format, sampling_frequency, sound_track, decode, bit_rate }
+          info.audio = message[key].trim(); 
+        } else {
+          info[key.trim()] = message[key].trim();
+        }
+      })
+      console.log('success', info)
+    } catch(err) {
+      console.log('fail', err);
+      info = {};
+    }
   });
   return info;
 }
